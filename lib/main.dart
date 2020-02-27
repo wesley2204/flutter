513457bbb5bb3935a8399852614c3ccca719a63c
wesley2204 <wk_2204@outlook.com>
2020-02-27 13:27:19 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main()=> runApp(
+  MaterialApp(
+
+    home: Center(
+      child: Container(
+        color:Colors.deepPurple,
+        child: Text("To com fome, e nao tem comida!",
+            style:TextStyle(color:Colors.black)),
+      ),
+    ),
+  ),
+);
